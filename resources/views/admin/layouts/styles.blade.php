7ee1376a1bbb9206e8d1a2f9bc9d50c062fb932f
novilfahlevy <novilfreon@gmail.com>
2024-07-03 20:02:10 +0800

    sliced admin dashboard

---
+++ resources/views/admin/layouts/styles.blade.php
@@ -0,0 +1,15 @@
+<!-- Vendors styles-->
+<link rel="stylesheet" href="vendors/simplebar/css/simplebar.css">
+<link rel="stylesheet" href="assets/admin/css/vendors/simplebar.css">
+
+<!-- Main styles for this application-->
+<link href="assets/admin/css/style.css" rel="stylesheet">
+
+<!-- We use those styles to show code examples, you should remove them in your application.-->
+<link href="assets/admin/css/examples.css" rel="stylesheet">
+
+<!-- We use those styles to style Carbon ads and CoreUI PRO banner, you should remove them in your application.-->
+<link href="assets/admin/css/ads.css" rel="stylesheet">
+<script src="assets/admin/js/config.js"></script>
+<script src="assets/admin/js/color-modes.js"></script>
+<link href="vendors/@coreui/chartjs/css/coreui-chartjs.css" rel="stylesheet">
