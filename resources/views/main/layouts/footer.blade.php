0d1b1a497b10b8edc6f81825f70b2de1df193164
novilfahlevy <novilfreon@gmail.com>
2024-07-09 14:48:32 +0800

    add footer

--- resources/views/main/layouts/footer.blade.php
+++ resources/views/main/layouts/footer.blade.php
@@ -1,50 +1,58 @@
 <footer id="footer" class="footer light-background">
   <div class="container footer-top">
-    <div class="row gy-4">
-      <div class="col-lg-5 col-md-12 footer-about">
-        <a href="index.html" class="logo d-flex align-items-center">
-          <span class="sitename">OnePage</span>
-        </a>
-        <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-        <div class="social-links d-flex mt-4">
-          <a href=""><i class="bi bi-twitter-x"></i></a>
-          <a href=""><i class="bi bi-facebook"></i></a>
-          <a href=""><i class="bi bi-instagram"></i></a>
-          <a href=""><i class="bi bi-linkedin"></i></a>
+    <div class="row justify-content-between gy-4">
+      <div class="col-lg-5 col-12 footer-about d-flex items-start">
+        <div>
+          <img src="assets/main/img/logo-pemkot.png" class="me-3" alt="Pemerintah Kota Samarinda">
+        </div>
+        <div>
+          <p>Pemerintah Kota Samarinda</p>
+          <a href="index.html" class="logo d-flex align-items-center">
+            <span class="sitename">Kelurahan Lempake</span>
+          </a>
+          <p>JL. Poros Kebon Agung No.98 Kel. Lempake Kec. Samarinda Utara.</p>
+          <p>2017-2024 © Pemerintah Situs Resmi Kelurahan Lempake</p>
+          <div class="social-links d-flex mt-4">
+            <a href=""><i class="bi bi-instagram"></i></a>
+          </div>
         </div>
       </div>
-
-      <div class="col-lg-2 col-6 footer-links">
-        <h4>Useful Links</h4>
+      <hr class="d-lg-none">
+      <div class="col-lg-3 col-12 footer-links">
+        <h4>Profil Kelurahan</h4>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About us</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Terms of service</a></li>
-          <li><a href="#">Privacy policy</a></li>
+          <li class="d-flex justify-content-between">
+            <p class="mb-0">Kecamatan</p>
+            <p class="mb-0">Samarinda Utara</p>
+          </li>
+          <li class="d-flex justify-content-between">
+            <p class="mb-0">Tipologi</p>
+            <p class="mb-0"></p>
+          </li>
+          <li class="d-flex justify-content-between">
+            <p class="mb-0">Luas Wilayah</p>
+            <p class="mb-0">3.450, 17 Ha</p>
+          </li>
+          <li class="d-flex justify-content-between">
+            <p class="mb-0">Populasi</p>
+            <p class="mb-0">20.679 Orang</p>
+          </li>
         </ul>
       </div>
-
-      <div class="col-lg-2 col-6 footer-links">
-        <h4>Our Services</h4>
+      <hr class="d-lg-none">
+      <div class="col-lg-3 col-12 footer-links">
+        <h4>Kontak Kami</h4>
         <ul>
-          <li><a href="#">Web Design</a></li>
-          <li><a href="#">Web Development</a></li>
-          <li><a href="#">Product Management</a></li>
-          <li><a href="#">Marketing</a></li>
-          <li><a href="#">Graphic Design</a></li>
+          <li class="d-flex justify-content-between">
+            <p class="mb-0">Telepon</p>
+            <p class="mb-0">(0541) - 280081</p>
+          </li>
+          <li class="d-flex justify-content-between">
+            <p class="mb-0">Email</p>
+            <p class="mb-0">kelurahanlempake98@gmail.com</p>
+          </li>
         </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-        <h4>Contact Us</h4>
-        <p>A108 Adam Street</p>
-        <p>New York, NY 535022</p>
-        <p>United States</p>
-        <p class="mt-4"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-        <p><strong>Email:</strong> <span>info@example.com</span></p>
-      </div>
-
+      </div>  
     </div>
   </div>
 
