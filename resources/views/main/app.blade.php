bcb7e716a482b09e7f982ce6b9cb14e526eb771b
novilfahlevy <novilfreon@gmail.com>
2024-07-11 11:31:16 +0800

    add headmane photo

--- resources/views/main/app.blade.php
+++ resources/views/main/app.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section id="hero" class="hero section">
-  <img src="assets/main/img/hero-bg-abstract.jpg" alt="" data-aos="fade-in" class="">
+  <img src="assets/main/img/hero-bg-abstract.jpg" id="background-image" alt="Background" data-aos="fade-in" class="">
 
   <div class="container">
     <div class="row justify-content-center" data-aos="zoom-in" data-aos-delay="50" style="margin-top: 50px; margin-bottom: 180px;">
@@ -12,9 +12,22 @@
       </div>
     </div>
     
-    <div class="card" data-aos="zoom-in" data-aos-delay="50">
-      <div class="card-body">
-        <iframe style="border:0; width: 100%; height: 400px;" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3989.7001609455933!2d117.19195617447825!3d-0.44095703528540797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sJL.%20Poros%20Kebon%20Agung%20No.98%20Kel.%20Lempake%20Kec.%20Samarinda%20Utara.!5e0!3m2!1sid!2sid!4v1720510262012!5m2!1sid!2sid" frameborder="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="row" id="headman">
+      <div class="d-none col-lg-3 d-lg-block">
+        <div class="card">
+          <img src="{{ url('assets/main/img/lempake-headman-photo.jpeg') }}" class="rounded-top" alt="Lurah Lempake">
+          <div class="card-body text-center py-2">
+            <p class="title mb-1">MUSLIADI, S.SOS</p>
+            <p class="description mb-2">Lurah Lempake</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-9">
+        <div class="card" data-aos="zoom-in" data-aos-delay="50">
+          <div class="card-body">
+            <iframe style="border:0; width: 100%; height: 400px;" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3989.7001609455933!2d117.19195617447825!3d-0.44095703528540797!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sJL.%20Poros%20Kebon%20Agung%20No.98%20Kel.%20Lempake%20Kec.%20Samarinda%20Utara.!5e0!3m2!1sid!2sid!4v1720510262012!5m2!1sid!2sid" frameborder="0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
       </div>
     </div>
 
