a75187045430199ad5df1951c6c464cb39234b8d
novilfahlevy <novilfreon@gmail.com>
2024-07-12 11:55:09 +0800

    navbar logo

--- resources/views/main/layouts/navbar.blade.php
+++ resources/views/main/layouts/navbar.blade.php
@@ -1,40 +1,21 @@
 <header id="header" class="header d-flex align-items-center sticky-top">
   <div class="container-fluid container-xl position-relative d-flex align-items-center">
-    <a href="index.html" class="logo d-flex align-items-center me-auto">
-      <!-- Uncomment the line below if you also wish to use an image logo -->
-      <!-- <img src="assets/img/logo.png" alt=""> -->
-      <h1 class="sitename">OnePage</h1>
+    <a href="{{ url('/') }}" class="logo d-flex align-items-center me-auto select-none">
+      <img src="{{ asset('assets/main/img/logo-pemkot.png') }}" alt="Pemerintah kota Samarinda">
+      <div>
+        <p class="sitename mb-1 fs-6 fw-light">Pemerintah kota Samarinda</p>
+        <h1 class="fs-6 mb-0 fw-bold">Kelurahan Lempake</h1>
+      </div>
     </a>
 
     <nav id="navmenu" class="navmenu">
       <ul>
-        <li><a href="#hero" class="active">Home<br></a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#team">Team</a></li>
-        <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-          <ul>
-            <li><a href="#">Dropdown 1</a></li>
-            <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-              <ul>
-                <li><a href="#">Deep Dropdown 1</a></li>
-                <li><a href="#">Deep Dropdown 2</a></li>
-                <li><a href="#">Deep Dropdown 3</a></li>
-                <li><a href="#">Deep Dropdown 4</a></li>
-                <li><a href="#">Deep Dropdown 5</a></li>
-              </ul>
-            </li>
-            <li><a href="#">Dropdown 2</a></li>
-            <li><a href="#">Dropdown 3</a></li>
-            <li><a href="#">Dropdown 4</a></li>
-          </ul>
-        </li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#hero" class="active">Profil<br></a></li>
+        <li><a href="#about">Informasi</a></li>
+        <li><a href="#services">Media</a></li>
+        <li><a href="#portfolio">UMKM</a></li>
       </ul>
       <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
     </nav>
-
-    <a class="btn-getstarted" href="#about">Get Started</a>
   </div>
 </header>
