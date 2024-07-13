2d25b25dc3d0094602666c45f8437e6905bd92a1
novilfahlevy <novilfreon@gmail.com>
2024-07-13 19:58:49 +0800

    docker

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -9,8 +9,8 @@
     <title>Index - OnePage Bootstrap Template</title>
 
     <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
     
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
@@ -18,14 +18,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{ url('assets/main/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/main/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/main/vendor/aos/aos.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/main/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/main/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/main/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/main/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/main/vendor/aos/aos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/main/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/main/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
     <!-- Main CSS File -->
-    <link href="{{ url('css/main/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main/app.css') }}" rel="stylesheet">
 
     <!-- =======================================================
     * Template Name: OnePage
@@ -54,16 +54,16 @@
     {{-- <div id="preloader"></div> --}}
 
     <!-- Vendor JS Files -->
-    <script src="{{ url('assets/main/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ url('assets/main/vendor/php-email-form/validate.js') }}"></script>
-    <script src="{{ url('assets/main/vendor/aos/aos.js') }}"></script>
-    <script src="{{ url('assets/main/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-    <script src="{{ url('assets/main/vendor/glightbox/js/glightbox.min.js') }}"></script>
-    <script src="{{ url('assets/main/vendor/swiper/swiper-bundle.min.js') }}"></script>
-    <script src="{{ url('assets/main/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
-    <script src="{{ url('assets/main/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/purecounter/purecounter_vanilla.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/imagesloaded/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/main/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
 
     <!-- Main JS File -->
-    <script src="{{ url('js/main/app.js') }}"></script>
+    <script src="{{ asset('js/main/app.js') }}"></script>
   </body>
 </html>
